94ea31c7bb9f7b484525d3a370a14efb1b8ef4ef
Dusan Vesic <dusan.vesic10@gmail.com>
2021-08-27 16:40:21 +0200

    new template

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,5 +1,189 @@
 <template>
   <div>
-    <h1>Products page</h1>
+    <v-container>
+      <div class="row">
+        <div
+         class="col-md-3 col-sm-3 col-xs-12"
+        >
+          <v-card outlined>
+            <v-card-title>Filters</v-card-title>
+            <v-divider></v-divider>
+            <template>
+              <v-treeview :items="items" :open="[1]" :active="[5]" :selected-color="'#fff'" activatable open-on-click dense></v-treeview>
+            </template>
+            <v-divider></v-divider>
+            <v-card-title>Price</v-card-title>
+            <v-range-slider
+              v-model="range"
+              :max="max"
+              :min="min"
+              :height="10"
+              class="align-center"
+              dense
+            ></v-range-slider>
+            <v-row class="pa-2" dense>
+              <v-col cols="12" sm="5">
+                <v-text-field
+                  :value="range[0]"
+                  label="Min"
+                  outlined
+                  dense
+                  @change="$set(range, 0, $event)"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="2">
+                <p class="pt-2 text-center">TO</p>
+              </v-col>
+              <v-col cols="12" sm="5">
+                <v-text-field
+                  :value="range[1]"
+                  label="Max"
+                  outlined
+                  dense
+                  @change="$set(range, 1, $event)"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-divider></v-divider>
+            <v-card-title class="pb-0">Customer Rating</v-card-title>
+            <v-container class="pt-0"  fluid>
+              <v-checkbox append-icon="mdi-star" label="4 & above" hide-details dense></v-checkbox>
+              <v-checkbox append-icon="mdi-star" label="3 & above" hide-details dense></v-checkbox>
+              <v-checkbox append-icon="mdi-star" label="2 & above" hide-details dense></v-checkbox>
+              <v-checkbox append-icon="mdi-star" label="1 & above" hide-details dense></v-checkbox>
+            </v-container>
+            <v-divider></v-divider>
+            <v-card-title class="pb-0">Size</v-card-title>
+            <v-container class="pt-0" fluid>
+              <v-checkbox  label="XS" hide-details dense></v-checkbox>
+              <v-checkbox  label="S" hide-details dense></v-checkbox>
+              <v-checkbox  label="M" hide-details dense></v-checkbox>
+              <v-checkbox  label="L" hide-details dense></v-checkbox>
+              <v-checkbox  label="XL" hide-details dense></v-checkbox>
+              <v-checkbox  label="XXL" hide-details dense></v-checkbox>
+              <v-checkbox  label="XXXL" hide-details dense></v-checkbox>
+            </v-container>
+
+          </v-card>
+        </div>
+        <div
+          class="col-md-9 col-sm-9 col-xs-12"
+        >
+
+          <v-breadcrumbs class="pb-0" :items="breadcrums"></v-breadcrumbs>
+
+          <v-row dense>
+            <v-col cols="12" sm="8" class="pl-6 pt-6">
+              <small>Showing 1-12 of 200 products</small>
+            </v-col>
+            <v-col cols="12" sm="4">
+              <v-select class="pa-0" v-model="select" :items="options" style="margin-bottom: -20px;" outlined dense></v-select>
+            </v-col>
+          </v-row>
+
+          <v-divider></v-divider>
+
+          <div class="row text-center">
+            <div class="col-md-3 col-sm-6 col-xs-12" :key="product.id" v-for="product in products">
+              <product
+                :id="product.id"
+                :category="product.category"
+                :name="product.name"
+                :price="product.price"
+                :totalAvailable="product.totalAvailable"
+                :brand="product.brand"
+              ></product>
+            </div>
+          </div>
+          <div class="text-center mt-12">
+            <v-pagination
+              v-model="page"
+              :length="6"
+            ></v-pagination>
+          </div>
+        </div>
+      </div>
+    </v-container>
   </div>
 </template>
+
+<script>
+    import Product from '../components/products/ProductCard.vue'
+
+    export default {
+        components: {
+          Product
+        },
+        data: () => ({
+            range: [0, 10000],
+            select:'Popularity',
+            options: [
+                'Default',
+                'Popularity',
+                'Relevance',
+                'Price: Low to High',
+                'Price: High to Low',
+            ],
+            page:1,
+            breadcrums: [
+                {
+                    text: 'Home',
+                    disabled: false,
+                    href: 'breadcrumbs_home',
+                },
+                {
+                    text: 'Clothing',
+                    disabled: false,
+                    href: 'breadcrumbs_clothing',
+                },
+                {
+                    text: 'T-Shirts',
+                    disabled: true,
+                    href: 'breadcrumbs_shirts',
+                },
+            ],
+            min:0,
+            max:10000,
+            items: [
+                {
+                    id: 2,
+                    name: 'Shoes',
+                    children: [
+                        { id: 2, name: 'Casuals' },
+                        { id: 3, name: 'Formals' },
+                        { id: 4, name: 'Sneakers' },
+                    ],
+                },
+                {
+                    id: 1,
+                    name: 'Clothing',
+                    children: [
+                        { id: 5, name: 'Shirts' },
+                        { id: 6, name: 'Tops' },
+                        { id: 7, name: 'Tunics' },
+                        { id: 8, name: 'Bodysuit' },
+                    ],
+                }
+            ],
+            products:[]
+        }),
+        created() {
+          this.$http.get("products")
+          .then(res => {
+            this.products = res.data;
+          })
+          .catch(err => console.log(err));
+        }
+    }
+</script>
+
+<style>
+  .v-card--reveal {
+    align-items: center;
+    bottom: 0;
+    justify-content: center;
+    opacity: .8;
+    position: absolute;
+    width: 100%;
+  }
+</style>
---
+++ src/components/common/Pagination.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-pagination
+        v-model="page"
+        :length="6"
+    ></v-pagination>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
